06d01fb1b571de113cc827da94c93a6ec8a2dd3d
rahman9868 <rahman9868@gmail.com>
2020-08-13 13:24:53 +0700

    Flutter-Beginner version 0.0.2
    - Image & Assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-   appBar: AppBar(
-     title: Text('Cr First App'),
-     centerTitle: true,
-     backgroundColor: Colors.purple[300],
-   ),
-    body: Center(
-      child: Text(
-          'Hello Cr',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower'
-        ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.purple[400],
-    ),
-  )
+  home: Home()
 ));
 
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cr First App'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[300],
+      ),
+      body: Center(
+        child: Image.asset('assets/image3.jpg') //from local asset
+        //child: Image.network('http://....') from internet
+
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Tap'),
+        backgroundColor: Colors.purple[400],
+      ),
+    );
+  }
+}
