d84866584b1ea52ea2cded6f6d51ab5e6616e87a
rahman9868 <rahman9868@gmail.com>
2020-08-13 08:46:56 +0700

    Flutter-Beginner version 0.0.1
    - Creating App
    - Scaffold & AppBar
    - Colours & Fonts

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+   appBar: AppBar(
+     title: Text('Cr First App'),
+     centerTitle: true,
+     backgroundColor: Colors.purple[300],
+   ),
+    body: Center(
+      child: Text(
+          'Hello Cr',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower'
+        ),
+      ),
+
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('Click'),
+      backgroundColor: Colors.purple[400],
+    ),
+  )
+));
+
